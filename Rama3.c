74d2b5508e5bd669431060f88925dd66ab1ab57f
Gabbyaam <111288008+Gabbyaam@users.noreply.github.com>
2022-08-17 14:12:38 -0500

    R3c

---
+++ Rama3.c
@@ -0,0 +1,14 @@
+public class Main 
+{
+    static void func_double(double x)
+    {
+        System.out.print(x);
+    }
+}
+
+static void main(String [] args)
+{
+    System.out.println("Práctica 2");
+    fun_int(55);
+    func_double(22.44);
+}
