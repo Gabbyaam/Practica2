bc9905c288e2f1abc28c20f59c23f11fafc5959a
Gabbyaam <111288008+Gabbyaam@users.noreply.github.com>
2022-08-17 14:08:04 -0500

    R2 c

---
+++ Rama2.c
@@ -0,0 +1,13 @@
+public class Main 
+{
+    static void func_int(int x)
+    {
+        Syste.out.print(x);
+    }
+}
+
+static void main(String [] args)
+{
+    System.out.println("Práctica 2");
+    fun_int(55);
+}
