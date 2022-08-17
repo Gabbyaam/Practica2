4ba00de45f0aef7e41b260f64d45927b1c4fa1ab
Gabbyaam <111288008+Gabbyaam@users.noreply.github.com>
2022-08-17 14:15:37 -0500

    R4 c

---
+++ Rama4.c
@@ -0,0 +1,15 @@
+public class Main 
+{
+    static void func_float(float x)
+    {
+        System.out.print(x);
+    }
+}
+
+static void main(String [] args)
+{
+    System.out.println("Práctica 2");
+    fun_int(55);
+    func_double(22.44);
+    func_float(444.2222222);
+}
